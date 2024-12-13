677adf7a3f784a2741c77a53ef8f1f06d09ca46c
Martiynz <143968800+Martiynz@users.noreply.github.com>
2024-12-12 22:49:47 -0600

    Enlazar viewModels

--- Examen-Mvvm/Views/Des.xaml.cs
+++ Examen-Mvvm/Views/Des.xaml.cs
@@ -1,9 +1,14 @@
+using Examen_Mvvm.ViewModels;
+
 namespace Examen_Mvvm.Views;
 
 public partial class Des : ContentPage
 {
+	DesViewModels ViewModels;
 	public Des()
 	{
 		InitializeComponent();
+		ViewModels = new DesViewModels();
+		BindingContext = ViewModels;
 	}
 }
